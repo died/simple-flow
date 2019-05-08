09ce58b8614cba33a545e02a546b2980495032c5
Died <doomdied@gmail.com>
2019-05-08 11:21:48 +0800

    update it for bootstrap card
    update it, so the flow can fit bootstrap card.

--- src/js/simple-flow.js
+++ src/js/simple-flow.js
@@ -3,10 +3,11 @@
 
     var pluginName = "SimpleFlow",
         defaults = {
+            rowHeight: 0,
             lineWidth: 2,
-            lineSpacerWidth: 15,
-            lineColour: '#91acb3',
-            canvasElm: '.canvas'
+            lineSpacerWidth: 30,
+            lineColour: "#91acb3",
+            canvasElm: ".canvas"
         };
 
     function Plugin ( element, options ) {
@@ -17,7 +18,8 @@
         this.init();
     }
 
-    $.extend( Plugin.prototype, {
+    $.extend(Plugin.prototype, {
+        
         init: function() {
             this.drawLines();
 
@@ -28,47 +30,58 @@
             $(window).resize(function() {
                if(this.resizeTO) clearTimeout(this.resizeTO);
                this.resizeTO = setTimeout(function() {
-                   $(this).trigger('resizeEnd');
+                   $(this).trigger("resizeEnd");
                }, 250);
             });
 
-            $(window).bind('resizeEnd', function() {
+            $(window).bind("resizeEnd", function() {
                that.drawLines();
             });
         },
 
         drawLines: function() {
             // remove old svgs
-            $(this.settings['canvasElm'] + ' ' + 'svg.simple-flow').remove()
+            $(this.settings["canvasElm"] + " " + "svg.simple-flow").remove();
 
             var uuid = this.getUuid();
 
             // define common elements
             var line = '<svg class="simple-flow simple-flow-defs">' +
-                '<defs>' +
+                "<defs>" +
                   '<marker id="arrowhead-' + uuid + '" viewBox="0 0 10 10" refX="8" refY="5"' +
                       'markerUnits="strokeWidth" markerWidth="8" markerHeight="6" orient="auto">' +
-                    '<path d="M 0 0 L 10 5 L 0 10 z" stroke="none" fill="' + this.settings['lineColour'] + '"/>' +
-                  '</marker>' +
-                '</defs>' +
-                '</svg>';
-            $(this.settings['canvasElm']).append(line);
+                    '<path d="M 0 0 L 10 5 L 0 10 z" stroke="none" fill="' + this.settings["lineColour"] + '"/>' +
+                  "</marker>" +
+                "</defs>" +
+                "</svg>";
+            $(this.settings["canvasElm"]).append(line);
 
             var $elements = $(this.element);
 
-            for (var i = 0; i < $elements.length; i++){
+            for (let i = 0; i < $elements.length; i++){
                 var thisElm =  $elements.eq(i),
                     nextElm = $elements.eq(i+1);
                 this.drawLine(thisElm, nextElm, uuid);
-            };
+            }
+
+            //make space less
+            $(".simple-flow-defs").height(10);
         },
 
-        drawLine: function(thisElm, nextElm, uuid){
+        drawLine: function (thisElm, nextElm, uuid) {
+
             var thisElmParent = thisElm.parent(),
                 nextElmParent = nextElm.parent();
 
+            //row height
+            var rowHeight = this.settings["rowHeight"];
+            if (thisElmParent.height() > rowHeight) {
+                rowHeight = thisElmParent.height();
+                this.settings["rowHeight"] = rowHeight;
+            }
+
             // only continue if there's a next element.
-            if (typeof nextElm.position() !== 'undefined') {
+            if (typeof nextElm.position() !== "undefined") {
                 // calculate all of the positions relative to the two objects
                 var thisElmMiddle = thisElm.outerHeight(true) / 2,
                     nextElmMiddle = nextElm.outerHeight(true) / 2;
@@ -82,33 +95,35 @@
                 var thisRight = thisElmParent.position().left + thisElm.outerWidth(true) + thisParentPadding,
                     nextLeft = nextElmParent.position().left + nextParentPadding;
 
-                var farLeftX = nextLeft - this.settings['lineSpacerWidth'],
-                    farRightX = thisRight + this.settings['lineSpacerWidth'],
-                    lineInBetweenY = (thisElmY + nextElmY) / 2;
-
+                var farLeftX = nextLeft - this.settings["lineSpacerWidth"];
+                var farRightX = thisRight + this.settings["lineSpacerWidth"];
+                //var lineInBetweenY = (thisElmY + nextElmY) / 2;
+                var lineInBetweenY = rowHeight + parseInt(thisElmParent.css("margin-bottom")) / 2;
                 // if the object is on the same line, the the coords are different
                 // to if they're on separate lines.
-                if (thisElmY == nextElmY) {
+                let coords;
+                if (thisElmY === nextElmY) {
                     // same row
-                    var coords = thisRight + ',' + thisElmY + ' ' + nextLeft + ',' + nextElmY;
+                    coords = thisRight + "," + thisElmY + " " + nextLeft + "," + nextElmY;
 
                 } else {
                     // differernt rows
-                    var coords = thisRight + ',' + thisElmY + ' ' + farRightX + ',' + thisElmY +
-                        ' ' + farRightX + ',' + lineInBetweenY + ' ' + farLeftX + ',' +
-                        lineInBetweenY + ' ' + farLeftX + ',' + nextElmY + ' ' + nextLeft +
-                        ',' + nextElmY;
+                    coords = thisRight + "," + thisElmY + " " + farRightX + "," + thisElmY +
+                        " " + farRightX + "," + lineInBetweenY + " " + farLeftX + "," +
+                        lineInBetweenY + " " + farLeftX + "," + nextElmY + " " + nextLeft +
+                        "," + nextElmY;
+                    this.settings["rowHeight"] = 0;
                 }
 
                 // create line svg
                 var line = '<svg class="simple-flow simple-flow-line">' +
                     '<path d="M' + coords +
-                      '"style="fill:none;stroke:' + this.settings['lineColour'] + ';stroke-width:' + this.settings['lineWidth'] +
-                      ';marker-end:url(#arrowhead-' + uuid + ');" />' +
-                    '</svg>';
+                      '"style="fill:none;stroke:' + this.settings["lineColour"] + ";stroke-width:" + this.settings["lineWidth"] +
+                      ";marker-end:url(#arrowhead-" + uuid + ');" />' +
+                    "</svg>";
 
                 // append to canvas
-                $(this.settings['canvasElm']).append(line);
+                $(this.settings["canvasElm"]).append(line);
             }
         },
         getUuid: function() {
@@ -117,8 +132,8 @@
                     .toString(16)
                     .substring(1);
             }
-            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-                s4() + '-' + s4() + s4() + s4();
+            return s4() + s4() + "-" + s4() + "-" + s4() + "-" +
+                s4() + "-" + s4() + s4() + s4();
         }
     });
 
